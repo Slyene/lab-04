c567c86538c56d22eb7f8385a71d3b30863f0d29
Slyene <deerektor@gmail.com>
2020-10-06 16:34:34 +0300

    .
    .

--- lab-04.lua
+++ lab-04.lua
@@ -1,3 +1,5 @@
+#!/usr/bin/lua5.3
+--Chesnokov 474
 lgi = require 'lgi'
 
 gtk = lgi.Gtk
@@ -6,27 +8,27 @@ pixbuf = lgi.GdkPixbuf.Pixbuf
 gtk.init()
 
 bld = gtk.Builder()
-bld::add_from_file('lab-04.glade')
-
+bld:add_from_file('lab-04.glade')
+--Chesnokov 474
 ui = bld.objects
 
 function ui.btnAdd:on_clicked(...)
   name = ui.entName.text
-  value = tonumber(ui.txtValue.text)
+  value = tonumber(ui.entValue.text)
   
   px = pixbuf.new_from_file('img-blueberry.png')
   
   i = ui.mdlItems:append()
   ui.mdlItems[i] = {[1] = name, [2] = value, [3] = px}
 end
-
+--Chesnokov 474
 rdr_txt = gtk.CellRendererText {}
 rdr_pix = gtk.CellRendererPixbuf {}
 
-c1 = gtk. TreeViewColumn { title = 'Name', {rdr_txt, { text = 1 }}}
-c2 = gtk. TreeViewColumn { title = 'Value', {rdr_txt, { text = 2 }}}
-c3 = gtk. TreeViewColumn { title = 'Image', {rdr_pix, { pixbuf = 3 }}}
-
+c1 = gtk.TreeViewColumn { title = 'Name', {rdr_txt, { text = 1 }}}
+c2 = gtk.TreeViewColumn { title = 'Value', {rdr_txt, { text = 2 }}}
+c3 = gtk.TreeViewColumn { title = 'Image', {rdr_pix, { pixbuf = 3 }}}
+--Chesnokov 474
 ui.lstItems:append_column(c1)
 ui.lstItems:append_column(c2)
 ui.lstItems:append_column(c3)
